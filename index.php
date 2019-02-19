f5231e598eea6461dfc7e89758ceb1bc34af9c25
Raphael Moraes <redney.sm@gmail.com>
2019-02-19 06:14:47 -0300

    painel adm e tpl adm

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -11,14 +12,15 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	//echo "OK";
-	//$sql = new Hcode\DB\Sql();
-
-	//$results = $sql->select("SELECT * FROM tb_users");
-
-	//echo json_encode($results);
-
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
